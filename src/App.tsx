0208a6cfdc187df3adbcd2c9e94e3562a5917c5c
Marco Ordonez <marco@evolbit.net>
2025-06-23 14:05:01 +0200

    feat(style): fix for height

--- src/App.tsx
+++ src/App.tsx
@@ -149,7 +149,7 @@ export default function App() {
             ) : (
               <div className="flex h-full w-full flex-col">
                 <TopToolbar />
-                <div className="flex-1">
+                <div className="flex-1 min-h-0">
                   <Split className="h-full w-full min-h-0 flex-1">
                     {/* Sidebar izquierdo */}
                     <Split.Pane
